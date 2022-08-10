3a85d528d005a4fc16d53a7bc36cda97f7135cd9
Tomás Weigenast <tomaswegenast@gmail.com>
2022-08-10 19:04:54 -0300

    Add CommandResult

---
+++ LiteDB.Server/Base/CommandContext.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using LiteDB.Server.Base.Protos;
+
+namespace LiteDB.Server.Base
+{
+    /// <summary>
+    /// A class that holds information of incoming TcpClient messages
+    /// </summary>
+    internal class CommandContext
+    {
+        private readonly byte[] m_Buffer;
+        private readonly Command m_Command;
+
+        public CommandReceivedContext(byte[] buffer)
+        {
+            m_Buffer = buffer;
+            m_Command = Command.Parser.ParseFrom(buffer);
+        }
+    }
+}
---
+++ LiteDB.Server/Base/CommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using LiteDB.Server.Base.Protos;
+
+namespace LiteDB.Server.Base
+{
+    /// <summary>
+    /// The handler of a command operation
+    /// </summary>
+    internal class CommandHandler
+    {
+        private readonly Func<CommandContext, CommandResult> m_Handler;
+
+        public CommandHandler(Func<CommandContext, CommandResult> handler)
+        {
+            m_Handler = handler;
+        }
+    }
+
+    /// <summary>
+    /// The handler of a command operation which handles data too
+    /// </summary>
+    /// <typeparam name="TData">The type of data the handler handles.</typeparam>
+    internal class CommandHandler<TData> : CommandHandler
+    {
+        private readonly Func<CommandContext, TData, CommandResult> m_Handler;
+
+        public CommandHandler(Func<CommandContext, TData, CommandResult> handler) : base(handler)
+        {
+            m_Handler = handler;
+        }
+    }
+}
